2d91a35f704014128be58503cd131d4271375cb9
MyeongJun Go <myeongjun.ko@gmail.com>
2024-04-03 23:24:04 +0900

    first commit

---
+++ src/main/java/com/app/decaf/DecafApplication.java
@@ -0,0 +1,13 @@
+package com.app.decaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecafApplication.class, args);
+	}
+
+}
